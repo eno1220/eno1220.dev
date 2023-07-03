fc2eee9451d8111a1d74eddb1573d2e52c75b25c
eno1220 <eno1220dev@gmail.com>
2023-07-03 21:37:29 +0900

    feat: add og properties

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang='jp'>
-      <Head />
+      <Head>
+        <meta name='description' content='eno1220のポートフォリオサイト' />
+        <meta name='theme-color' content='#0E1117' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@1220_eno' />
+        <meta property='og:title' content='eno1220.dev' />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://eno1220.dev' />
+        <meta
+          property='og:description'
+          content='eno1220のポートフォリオサイト'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
