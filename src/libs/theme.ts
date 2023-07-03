ee7a3f8972f02e6bec985fd96a7737637b7ed575
eno1220 <eno1220dev@gmail.com>
2023-07-03 17:13:17 +0900

    feat: setup Chakra-ui

---
+++ src/libs/theme.ts
@@ -0,0 +1,47 @@
+import { Noto_Sans_JP } from 'next/font/google'
+
+import { extendBaseTheme } from '@chakra-ui/react'
+import {
+  Button as ButtonTheme,
+  Container as ContainerTheme,
+  Heading as HeadingTheme,
+  List as ListTheme,
+} from '@chakra-ui/theme/components'
+
+const notoSansJP = Noto_Sans_JP({
+  weight: ['400', '700', '800'],
+  style: 'normal',
+  display: 'swap',
+  preload: false,
+})
+
+const fonts = {
+  heading: notoSansJP.style.fontFamily,
+  body: notoSansJP.style.fontFamily,
+}
+
+const theme = extendBaseTheme({
+  components: {
+    Button: ButtonTheme,
+    Container: ContainerTheme,
+    Heading: HeadingTheme,
+    List: ListTheme,
+  },
+  colors: {
+    blue: {
+      '50': '#eff6ff',
+      '100': '#dbeafe',
+      '200': '#bfdbfe',
+      '300': '#93c5fd',
+      '400': '#60a5fa',
+      '500': '#3b82f6',
+      '600': '#2563eb',
+      '700': '#1d4ed8',
+      '800': '#1e40af',
+      '900': '#1e3a8a',
+    },
+  },
+  fonts,
+})
+
+export default theme
